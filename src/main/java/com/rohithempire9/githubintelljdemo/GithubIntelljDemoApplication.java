052537b4aa83c3788a40f40a39f35f8293dc9de7
rohithempire9-hub <your-github-email@example.com>
2026-08-30 16:19:52 +0530

    init commit

---
+++ src/main/java/com/rohithempire9/githubintelljdemo/GithubIntelljDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rohithempire9.githubintelljdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntelljDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIntelljDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rohithempire9/githubintelljdemo/GithubIntelljDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohithempire9.githubintelljdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntelljDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
